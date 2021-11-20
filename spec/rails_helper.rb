a79dc7868565b8578fee7d31432fc96d5bd59b11
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-11-19 22:33:58 -0300

    setup(github): actions

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,8 +65,8 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   # config.filter_gems_from_backtrace("gem name")
 
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
+  Shoulda::Matchers.configure do |c|
+    c.integrate do |with|
       with.test_framework :rspec
       with.library :rails
     end
